1c88359da6598c6721516ad594555ae7e37d2fd3
mohamedsalahnassar <30360290+mohamedsalahnassar@users.noreply.github.com>
2025-08-20 19:47:54 +0400

    Set bundle identifier for macOS builds

--- ProjectPlannerApp/Sources/ProjectPlannerApp.swift
+++ ProjectPlannerApp/Sources/ProjectPlannerApp.swift
@@ -1,9 +1,19 @@
 import SwiftUI
 import CoreData
+#if os(macOS)
+import Foundation
+#endif
 
 @main
 @MainActor
 struct ProjectPlannerApp: App {
+    init() {
+        #if os(macOS)
+        // Assign a bundle identifier when running outside of an app bundle
+        Bundle.main.setValue("com.example.ProjectPlannerApp", forKey: "bundleIdentifier")
+        #endif
+    }
+
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
